04c212a293fbed5e918785474776350d77759162
MolnarZsofiaa <molnar.zsofia.23f@szlgbp.hu>
2024-06-09 16:03:31 +0200

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+const cim = document.querySelector('h1');
+const input = document.querySelector('input#nev');
+const button = document.querySelector('button#send');
+console.log(cim, input, button)
+
+button.addEventListener('click', onClick)
+
+function onClick (){
+    cim.innerHTML = `Hello, ${input.value}!`;
+}
+console.log(cim)
